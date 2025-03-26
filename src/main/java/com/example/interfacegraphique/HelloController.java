548de6939cd8687df6a2a5180200791b9b974686
Ry-Se-afk <sryane046@gmail.com>
2025-03-26 08:46:46 +0100

    image adaptée à la taille de la fenêtre

--- src/main/java/com/example/interfacegraphique/HelloController.java
+++ src/main/java/com/example/interfacegraphique/HelloController.java
@@ -2,13 +2,31 @@ package com.example.interfacegraphique;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private VBox vboxContainer;
+
+    @FXML
+    private ImageView backgroundImage;
+
+    @FXML
+    public void initialize() {
+        Image image = new Image(getClass().getResource("/images/fondApp.jpg").toExternalForm());
+        // Appliquer l'image à l'ImageView
+        backgroundImage.setImage(image);
+        backgroundImage.setPreserveRatio(false); // Conserve les proportions
+
+        // Adapter la taille de l'image à la VBox
+        backgroundImage.fitWidthProperty().bind(vboxContainer.widthProperty());
+        backgroundImage.fitHeightProperty().bind(vboxContainer.heightProperty());
+
+        // Permettre à l'image de s'étendre et de remplir l'espace
+        VBox.setVgrow(backgroundImage, Priority.ALWAYS);}
 }
